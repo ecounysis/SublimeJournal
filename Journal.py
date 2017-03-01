895f203b72e3d18e3c64310aa9525112eff52dbe
Eric Christensen <ericchris500@gmail.com>
2017-03-01 13:52:20 -0700

    more prompts and ideas

--- Journal.py
+++ Journal.py
@@ -11,17 +11,31 @@ class JournalCommand(sublime_plugin.TextCommand):
         year = time_tuple.tm_year
         day_of_year = time_tuple.tm_yday
         date_string = right_now.strftime("%m/%d/%Y %H:%M:%S")
+        
         settings = sublime.load_settings("Journal prompts.sublime-settings")
-        daily_prompts = settings.get("daily_prompts")
-        random_prompts = settings.get("random_prompts")
+        
+        #school_prompts = []
+        #school_prompts_list = settings.get("school_prompts_list")
+        #for pr in school_prompts_list:
+        #    school_prompts += settings.get(pr)
+        #school_prompt = school_prompts[random.randint(0, len(school_prompts) - 1)]
+
+        random_prompts = []
+        random_prompts_list = settings.get("random_prompts_list")
+        for pr in random_prompts_list:
+            random_prompts += settings.get(pr)
         random_prompt = random_prompts[random.randint(0, len(random_prompts) - 1)]
+        
+        daily_prompts = settings.get("daily_prompts")
         daily_prompt = daily_prompts[day_of_year - 1]
+        
+
         if (year % 4 == 0): # leap year
             if (day_of_year == 60): # leap day
                 daily_prompt = "Happy Leap Day! " + random_prompts[random.randint(0, len(random_prompts) - 1)]
             if (day_of_year > 60): # after leap day
                 daily_prompt = daily_prompts[day_of_year - 2]
-        header = "{}\n\nRandom Prompt:\n{}\n\nDaily Prompt:\n{}\n\n".format(date_string, random_prompt, daily_prompt)
+        header = "{}\n\nRandom Prompt:\n{}\n\nDaily Prompt:\n{}\n\n\n\n".format(date_string, random_prompt, daily_prompt)
 
         self.view.insert(edit, 0, header)
  
